6ae08420c305975b93ecdf8ab2ce2eedc4948367
Maria Pacana <maria.pacana@gmail.com>
2013-12-30 08:59:26 -0500

    Fixed score_sentence so it handles arrays and strings

--- lib/markovfun/trigram.rb
+++ lib/markovfun/trigram.rb
@@ -92,7 +92,7 @@ module Markovfun
     def self.score_sentence(sent, probs)
       total_surprise = 0
 
-      sent = sent[0..-2].split(" ").push(".")
+      sent = sent[0..-2].split(" ").push(".") if sent.class == String
       sent = [nil, nil] + sent + [nil]
 
       sent.zip(sent[1..-1], sent[2..-1]).each do |prev2, prev1, cur|
--- lib/markovfun/version.rb
+++ lib/markovfun/version.rb
@@ -1,3 +1,3 @@
 module Markovfun
-  VERSION = "0.0.3"
+  VERSION = "0.0.4"
 end
